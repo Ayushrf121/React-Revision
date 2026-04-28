e4cfefc14ea4dadc1451d4416c36347f96ba7ba0
Ayush Rawat <ayushrf@gmail.com>
2026-04-28 22:01:47 +0530

    main.jsx uploaded

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App3 from './App3.jsx'
 import App4 from './App4.jsx'
 import App5 from './App5.jsx'
 import App6 from './App6.jsx'
-import App7 from './App7.jsx'
+// import App7 from './App7.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
@@ -16,7 +16,7 @@ createRoot(document.getElementById('root')).render(
     // <App3 />
     // <App4 />
     // <App5 />
-    // <App6 />
-    <App7 />
+    <App6 />
+    // <App7 />
   // </StrictMode>,
 )
